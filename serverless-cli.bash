d18263b4b40f2e3b4cfbc4cad1465764827b8c17
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2020-06-30 12:39:12 -0300

    chore: add serverless cli commands

---
+++ serverless-cli.bash
@@ -0,0 +1,10 @@
+# Deploy
+sls deploy -v
+# Remove
+sls remove
+# Logs
+sls logs -f 
+# Create project with template
+sls create -t aws-nodejs-typescript --path 
+# Configure credentials AWS
+sls config credentials --provider aws --key= --secret 
